12e04c2f6a1cd5ce5a29aa2ad3f361df0df038e5
Heejae Kim <heejae.kim@navercorp.com>
2017-11-29 19:40:22 +0900

    test(all): update launcher to use mesa flag

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
     customLaunchers: {
       CustomChromeHeadless: {
         base: 'Chrome',
-        flags: ['--headless', '--remote-debugging-port=9222']
+        flags: ['--headless', '--remote-debugging-port=9222', '--use-gl=osmesa']
       }
     },
     reporters: ["mocha"],
