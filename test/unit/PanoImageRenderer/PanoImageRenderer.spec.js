23cad3d9fefa6ff21439b6ff9650332c5b7bc78a
Hee Jae Kim <heejae.kim@navercorp.com>
2017-12-05 14:26:16 +0900

    test(PanoImageRenderer): added test for automatic high dpi support (#76)
    ref #70 
    resolved #70

--- test/unit/PanoImageRenderer/PanoImageRenderer.spec.js
+++ test/unit/PanoImageRenderer/PanoImageRenderer.spec.js
@@ -1,10 +1,14 @@
 import PanoImageRenderer from "../../../src/PanoImageRenderer/PanoImageRenderer";
 import WebGLUtils from "../../../src/PanoImageRenderer/WebGLUtils";
 
-const IT = WebGLUtils.isWebGLAvailable() ? it : it.skip;
-console.log(WebGLUtils.isWebGLAvailable() ? "gl available" : "no gl");
-describe("PanoImageRenderer", function() {
-	describe("#constructor", function() {
+const WEBGL_AVAILABILITY = WebGLUtils.isWebGLAvailable();
+const IT = WEBGL_AVAILABILITY ? it : it.skip;
+describe("PanoImageRenderer", function () {
+	console.log(WEBGL_AVAILABILITY ? "gl available" : "no gl");
+	const deviceRatio = window.devicePixelRatio;
+	const suffix = `_${deviceRatio}x.png`;
+
+	describe("#constructor", function () {
 		IT("Instance", function() {
 			// Given
 			// When
@@ -39,7 +43,7 @@ describe("PanoImageRenderer", function() {
 				.then(() => {
 					inst.render(0, 0, 65);
 					// Then
-					compare("./base/test/img/test_equi_0_0_65.png", inst.canvas, function(pct) {
+					compare(`./base/test/img/test_equi_0_0_65${suffix}`, inst.canvas, function(pct) {
 						expect(pct).to.be.below(2);
 						done();
 					});
