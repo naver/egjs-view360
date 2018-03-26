0beab30cb9eb7c0893ba0d6f7c4a0305a26db5a8
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2018-03-26 18:30:17 +0900

    skip: remove useless

--- src/YawPitchControl/browser.js
+++ src/YawPitchControl/browser.js
@@ -7,7 +7,6 @@ const win = typeof window !== "undefined" && window.Math === Math ? window : typ
 win.Float32Array = (typeof win.Float32Array !== "undefined") ? win.Float32Array : win.Array;
 
 export {win as window};
-export const screen = win.screen;
 export const document = win.document;
 export const Float32Array = win.Float32Array;
 export const getComputedStyle = win.getComputedStyle;
