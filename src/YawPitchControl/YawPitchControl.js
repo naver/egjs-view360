d442ddb3c0dc5660edc8ca5fc3809c1dbc27aa63
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2018-03-29 15:11:34 +0900

    fix(YawPitchControl): null check
    Ref #181

--- src/YawPitchControl/YawPitchControl.js
+++ src/YawPitchControl/YawPitchControl.js
@@ -87,7 +87,6 @@ const YawPitchControl = class YawPitchControl extends Component {
 
 		this.axesPanInput = new RotationPanInput(this._element, {useRotation});
 		this.axesWheelInput = new WheelInput(this._element, {scale: 4});
-		// this.axesTiltMotionInput = SUPPORT_DEVICEMOTION ? new TiltMotionInput(this._element) : null;
 		this.axesTiltMotionInput = null;
 		this.axesPinchInput = SUPPORT_TOUCH ? new PinchInput(this._element, {scale: -1}) : null;
 		this.axesMoveKeyInput = new MoveKeyInput(this._element, {scale: [-6, 6]});
@@ -458,7 +457,7 @@ const YawPitchControl = class YawPitchControl extends Component {
 		event.pitch = pos.pitch;
 		event.fov = pos.fov;
 
-		if (opt.gyroMode === GYRO_MODE.VR) {
+		if (opt.gyroMode === GYRO_MODE.VR && this._deviceQuaternion) {
 			event.quaternion = this._deviceQuaternion.getCombinedQuaternion(pos.yaw, pos.pitch);
 		}
 		this.trigger("change", event);
