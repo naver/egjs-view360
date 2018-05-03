76b426bd129a2583c4325ffbfc14e055932674c3
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2018-05-03 15:17:48 +0900

    chore(all): add jsdoc for VERSION
    Ref #205

--- src/PanoViewer/index.js
+++ src/PanoViewer/index.js
@@ -7,4 +7,14 @@ export {
 	VERSION
 };
 
+/**
+ * Version info string
+ * @ko 버전정보 문자열
+ * @name VERSION
+ * @static
+ * @type {String}
+ * @example
+ * eg.view360.PanoViewer.VERSION;  // ex) 3.0.1
+ * @memberof eg.view360.PanoViewer
+ */
 PanoViewer.VERSION = VERSION;
