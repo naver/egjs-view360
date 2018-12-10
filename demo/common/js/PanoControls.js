7fea517a04d8a0da0b1de31ec7df7c206d3c3652
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2018-12-10 14:12:07 +0900

    chore(PanoViewer): add panoviewer controls
    Ref #250

---
+++ demo/common/js/PanoControls.js
@@ -0,0 +1,130 @@
+var PanoControls = (function() {
+	/**
+	 * 1. Direction Navigator
+	 * 2. Full Screen
+	 *
+	 * @param {HTMLElement} target
+	 */
+	function init(target, panoViewer) {
+		_initControlHTML(target);
+
+		/**
+		 * Navigator Handler
+		 */
+		var pieView = new PieView(document.querySelector(".camera"));
+		panoViewer.on({
+			"ready": function () {
+				// console.log("ready");
+				var yawRange = panoViewer.getYawRange();
+				pieView.setState(panoViewer.getYaw(), panoViewer._getHFov(), yawRange[1] - yawRange[0]);
+				showLoading(false);
+			},
+			"viewChange": function (e) {
+					var hfov = panoViewer._getHFov();
+					pieView.setState(e.yaw,  hfov);
+					// console.log("viewChange");
+			}
+		});
+
+		/**
+		 * Resize Handler
+		 */
+		window.addEventListener("resize", debounce(function() {
+			panoViewer.updateViewportDimensions();
+
+			var yaw = panoViewer.getYaw();
+			var hfov = panoViewer._getHFov();
+			pieView.setState(yaw,  hfov);
+		}));
+
+		/**
+		 * Full Screen Handler
+		 */
+		var originalContainer = target.parentElement;
+		var fullscreenContainer = document.getElementsByClassName("fullscreen-container")[0];
+
+		function changeMode(mode) {
+				var rootNode = container.parentNode.removeChild(container);
+				var requestFullscreen;
+
+				if (screenfull.enabled) {
+						requestFullscreen = screenfull.isFullscreen;
+				} else {
+						requestFullscreen = mode === "full";
+				}
+
+				if (requestFullscreen) {
+						fullscreenContainer.appendChild(rootNode);
+					fullscreenContainer.style.display = "block";
+				} else {
+						originalContainer.appendChild(rootNode);
+					fullscreenContainer.style.display = "none";
+				}
+
+				// resize event is not triggered.
+				!screenfull.enabled && panoViewer.updateViewportDimensions();
+		}
+
+		screenfull.enabled && screenfull.on("change", changeMode);
+
+		document.querySelector(".enterfs").addEventListener("click", function() {
+				if (screenfull.enabled) {
+						screenfull.request();
+				} else {
+						changeMode("full");
+				}
+		});
+
+		document.querySelector(".exitfs").addEventListener("click", function() {
+				if (screenfull.enabled) {
+						screenfull.exit();
+				} else {
+						changeMode("orignal");
+				}
+		});
+	}
+
+	function _initControlHTML(target) {
+		var controls = '\
+			<div class="image360_loading"><div class="image360_loadingBar"></div></div>\
+			<div class="panoviewer-control">\
+				<button class="enterfs">Enter Full Screen</button>\
+				<button class="exitfs">Exit Full Screen</button>\
+				<div class="camera"></div>\
+			</div>';
+
+		target.insertAdjacentHTML("beforeend", controls);
+		target.insertAdjacentHTML("afterend", '<div class="fullscreen-container"></div>');
+	}
+
+	function showLoading(isVisible) {
+		var visible = (isVisible == undefined || isVisible == true) ? true : false;
+		var loadingClassList = document.querySelector(".image360_loading").classList;
+
+		if (visible) {
+			loadingClassList.add("is-loading");
+		} else {
+			loadingClassList.remove("is-loading");
+		}
+	}
+
+	return {
+		init: init,
+		showLoading: showLoading
+	}
+})();
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
