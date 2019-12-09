e28e046272fa30416c320ce3cd23d9096c0b6977
WoodNeck <saiyu915@naver.com>
2019-12-09 18:13:54 +0900

    feat: use generic sensor polyfill in device quaternion

--- src/utils/mathUtil/quat.js
+++ src/utils/mathUtil/quat.js
@@ -224,4 +224,23 @@ quat.exactEquals = function (a, b) {
     return a[0] === b[0] && a[1] === b[1] && a[2] === b[2] && a[3] === b[3];
 };
 
+/**
+ * Calculates the inverse of a quat
+ *
+ * @param {quat} out the receiving quaternion
+ * @param {quat} a quat to calculate inverse of
+ * @returns {quat} out
+ */
+quat.invert = function (out, a) {
+	let a0 = a[0], a1 = a[1], a2 = a[2], a3 = a[3];
+	let dot = a0*a0 + a1*a1 + a2*a2 + a3*a3;
+	let invDot = dot ? 1.0/dot : 0;
+	// TODO: Would be faster to return [0,0,0,0] immediately if dot == 0
+	out[0] = -a0*invDot;
+	out[1] = -a1*invDot;
+	out[2] = -a2*invDot;
+	out[3] = a3*invDot;
+	return out;
+}
+
 export default quat;
