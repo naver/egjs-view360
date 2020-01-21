60cff818c6468fc4ce9ba15e8d4ef89b4ba7d184
WoodNeck <saiyu915@naver.com>
2020-01-21 18:28:59 +0900

    feat: enhance typings

--- declaration/SpinViewer.d.ts
+++ declaration/SpinViewer.d.ts
@@ -1,12 +1,22 @@
 import Component from "@egjs/component";
 
 declare class SpinViewer {
-  constructor(element: HTMLElement, param: object);
+  static VERSION: string;
+  constructor(element: HTMLElement, param: Partial<{
+    imageUrl: string;
+    rowCount: number;
+    colCount: number;
+    width: number | string;
+    height: number | string;
+    autoHeight: boolean;
+    colRow: number[];
+    scale: number;
+  }>);
   getAngle(): number;
   getScale(): number;
   setScale(scale: number): this;
-  spinBy(angle: number, param?: object): this;
-  spinTo(angle: number, param?: object): this;
+  spinBy(angle: number, param?: Partial<{ duration: number }>): this;
+  spinTo(angle: number, param?: Partial<{ duration: number }>): this;
 }
 
 export default SpinViewer;
