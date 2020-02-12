e3d0ebdae97588fd9a4b276234878f01dbe2f004
WoodNeck <saiyu915@naver.com>
2020-02-12 18:40:17 +0900

    Updates

--- common/js/PanoControls.js
+++ common/js/PanoControls.js
@@ -101,8 +101,75 @@ var PanoControls = (function() {
 		});
 
 		document.querySelector(".entervr").addEventListener("click", function() {
-			panoViewer.enterVR();
+			panoViewer.enterVR().catch(e => {
+				Swal.fire({
+					title: "Can't enter VR mode!",
+					text: e.message || e,
+					icon: "error"
+				});
+			});
 		});
+
+		// For iOS 13+
+		panoViewer.enableSensor().catch(() => {
+			Swal.fire({
+				title: "Permission needed! (iOS13+)",
+				icon: "question",
+				text: "Please give me a permission to access Device motion & Orientation :3",
+				showCancelButton: true,
+				reverseButtons: true,
+				confirmButtonText: "Allow",
+				cancelButtonText: "Deny"
+			}).then(result => {
+				if (result.value) {
+					// Granted
+					panoViewer.enableSensor().catch(() => {
+						Swal.fire(
+							"You've denied a permission!",
+							"You have to completely close out your browser and reconnect this page to enable sensor again!",
+							"error"
+						);
+					});
+				} else if (result.dismiss === Swal.DismissReason.cancel) {
+					// User dismissed
+				}
+			});
+		});
+
+		const iOSVersion = (() => {
+			if (/iP(hone|od|ad)/.test(navigator.platform)) {
+				// supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+				var v = navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
+				return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+			}
+		})();
+
+		// For iOS 12.2 ~ 13
+		if (iOSVersion && iOSVersion[0] === 12 && iOSVersion[1] >= 2) {
+			PanoViewer.isGyroSensorAvailable(available => {
+				if (!available) {
+					Swal.fire({
+						title: "Please enable the Motion Sensor! (iOS12.2~13)",
+						icon: "warning",
+						text: "This website requires a permission for your device sensor.",
+						html: `
+						<div style="text-align: left;">
+							<div>
+							1. Open <img src="https://developer.apple.com/design/human-interface-guidelines/ios/images/icons/app_icons/Settings_2x.png" width="20" /> <b>Settings</b>
+							</div>
+							<div>
+							2. Select <img src="https://km.support.apple.com/resources/sites/APPLE/content/live/IMAGES/0/IM26/en_US/safari-240.png" width="20" /> <b>Safari</b>
+							</div>
+							<div>
+							3. Enable <b>Motion & Orientation Access</b>
+							</div>
+							<div>4. Reload the page</div>
+						</div>
+						`
+					});
+				}
+			});
+		}
 	}
 
 	function _initControlHTML(target) {
