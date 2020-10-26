62a3b119ca57f1e1511d4d1f03f5fada521a6cc9
WoodNeck <saiyu915@naver.com>
2020-10-26 18:50:43 +0900

    feat: turning vr-related components to typescript

--- src/PanoImageRenderer/renderer/CubeRenderer.ts
+++ src/PanoImageRenderer/renderer/CubeRenderer.ts
@@ -5,249 +5,249 @@ import { util as mathUtil } from "../../utils/math-util.js";
 import { CubemapConfig } from "src/types.js";
 
 class CubeRenderer extends Renderer {
-	private static _VERTEX_POSITION_DATA: number[] | null = null;
+  private static _VERTEX_POSITION_DATA: number[] | null = null;
   private static _INDEX_DATA: number[] | null = null;
 
   public static extractOrder(imageConfig: CubemapConfig) {
-		return imageConfig.order || "RLUDBF";
-	}
+    return imageConfig.order || "RLUDBF";
+  }
 
-	public getVertexPositionData() {
-		CubeRenderer._VERTEX_POSITION_DATA =
-			CubeRenderer._VERTEX_POSITION_DATA !== null ? CubeRenderer._VERTEX_POSITION_DATA : [
-				// back
-				1, -1, 1,
-				-1, -1, 1,
-				-1, 1, 1,
-				1, 1, 1,
+  public getVertexPositionData() {
+    CubeRenderer._VERTEX_POSITION_DATA =
+      CubeRenderer._VERTEX_POSITION_DATA !== null ? CubeRenderer._VERTEX_POSITION_DATA : [
+        // back
+        1, -1, 1,
+        -1, -1, 1,
+        -1, 1, 1,
+        1, 1, 1,
 
-				// front
-				-1, -1, -1,
-				1, -1, -1,
-				1, 1, -1,
-				-1, 1, -1,
+        // front
+        -1, -1, -1,
+        1, -1, -1,
+        1, 1, -1,
+        -1, 1, -1,
 
-				// top
-				-1, 1, -1,
-				1, 1, -1,
-				1, 1, 1,
-				-1, 1, 1,
+        // top
+        -1, 1, -1,
+        1, 1, -1,
+        1, 1, 1,
+        -1, 1, 1,
 
-				// bottom
-				1, -1, -1,
-				-1, -1, -1,
-				-1, -1, 1,
-				1, -1, 1,
+        // bottom
+        1, -1, -1,
+        -1, -1, -1,
+        -1, -1, 1,
+        1, -1, 1,
 
-				// right
-				1, -1, -1,
-				1, -1, 1,
-				1, 1, 1,
-				1, 1, -1,
+        // right
+        1, -1, -1,
+        1, -1, 1,
+        1, 1, 1,
+        1, 1, -1,
 
-				// left
-				-1, -1, 1,
-				-1, -1, -1,
-				-1, 1, -1,
-				-1, 1, 1
-			];
+        // left
+        -1, -1, 1,
+        -1, -1, -1,
+        -1, 1, -1,
+        -1, 1, 1
+      ];
 
-		return CubeRenderer._VERTEX_POSITION_DATA;
-	}
+    return CubeRenderer._VERTEX_POSITION_DATA;
+  }
 
-	public getIndexData() {
-		if (CubeRenderer._INDEX_DATA) {
-			return CubeRenderer._INDEX_DATA;
-		}
+  public getIndexData() {
+    if (CubeRenderer._INDEX_DATA) {
+      return CubeRenderer._INDEX_DATA;
+    }
 
-		const indexData: number[] = [];
-		const vertexPositionData = this.getVertexPositionData();
+    const indexData: number[] = [];
+    const vertexPositionData = this.getVertexPositionData();
 
-		for (let i = 0; i < (vertexPositionData.length / 3); i += 4) {
-			indexData.push(
-				i,
-				i + 2,
-				i + 1,
-				i,
-				i + 3,
-				i + 2
-			);
-		}
+    for (let i = 0; i < (vertexPositionData.length / 3); i += 4) {
+      indexData.push(
+        i,
+        i + 2,
+        i + 1,
+        i,
+        i + 3,
+        i + 2
+      );
+    }
 
-		CubeRenderer._INDEX_DATA = indexData;
-		return indexData;
-	}
+    CubeRenderer._INDEX_DATA = indexData;
+    return indexData;
+  }
 
-	public getTextureCoordData(imageConfig: CubemapConfig) {
-		const vertexOrder = "BFUDRL";
-		const order = CubeRenderer.extractOrder(imageConfig);
-		const base = this.getVertexPositionData();
-		const tileConfig = this._extractTileConfig(imageConfig);
-		const elemSize = 3;
-		const vertexPerTile = 4;
-		const textureCoordData =
-			vertexOrder.split("")
-				.map(face => tileConfig[order.indexOf(face)])
-				.map((config, i) => {
-					const rotation = Math.floor(config.rotation / 90);
-					const ordermap_ = config.flipHorizontal ? [0, 1, 2, 3] : [1, 0, 3, 2];
+  public getTextureCoordData(imageConfig: CubemapConfig) {
+    const vertexOrder = "BFUDRL";
+    const order = CubeRenderer.extractOrder(imageConfig);
+    const base = this.getVertexPositionData();
+    const tileConfig = this._extractTileConfig(imageConfig);
+    const elemSize = 3;
+    const vertexPerTile = 4;
+    const textureCoordData =
+      vertexOrder.split("")
+        .map(face => tileConfig[order.indexOf(face)])
+        .map((config, i) => {
+          const rotation = Math.floor(config.rotation / 90);
+          const ordermap_ = config.flipHorizontal ? [0, 1, 2, 3] : [1, 0, 3, 2];
 
-					for (let r = 0; r < Math.abs(rotation); r++) {
-						if ((config.flipHorizontal && rotation > 0) ||
-							(!config.flipHorizontal && rotation < 0)) {
-							ordermap_.push(ordermap_.shift()!);
-						} else {
-							ordermap_.unshift(ordermap_.pop()!);
-						}
-					}
+          for (let r = 0; r < Math.abs(rotation); r++) {
+            if ((config.flipHorizontal && rotation > 0) ||
+              (!config.flipHorizontal && rotation < 0)) {
+              ordermap_.push(ordermap_.shift()!);
+            } else {
+              ordermap_.unshift(ordermap_.pop()!);
+            }
+          }
 
-					const elemPerTile = elemSize * vertexPerTile;
-					const tileVertex = base.slice(i * elemPerTile, i * elemPerTile + elemPerTile);
-					const tileTemp: number[][] = [];
+          const elemPerTile = elemSize * vertexPerTile;
+          const tileVertex = base.slice(i * elemPerTile, i * elemPerTile + elemPerTile);
+          const tileTemp: number[][] = [];
 
-					for (let j = 0; j < vertexPerTile; j++) {
-						tileTemp[ordermap_[j]] = tileVertex.splice(0, elemSize);
-					}
-					return tileTemp;
-				})
-				.join()
-				.split(",")
-				.map(v => parseInt(v, 10));
+          for (let j = 0; j < vertexPerTile; j++) {
+            tileTemp[ordermap_[j]] = tileVertex.splice(0, elemSize);
+          }
+          return tileTemp;
+        })
+        .join()
+        .split(",")
+        .map(v => parseInt(v, 10));
 
-		return textureCoordData;
-	}
+    return textureCoordData;
+  }
 
-	public getVertexShaderSource() {
-		return `
+  public getVertexShaderSource() {
+    return `
 attribute vec3 aVertexPosition;
 attribute vec3 aTextureCoord;
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
 varying highp vec3 vVertexDirectionVector;
 void main(void) {
-	vVertexDirectionVector = aTextureCoord;
-	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+  vVertexDirectionVector = aTextureCoord;
+  gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
 }`;
-	}
+  }
 
-	public getFragmentShaderSource() {
-		return `
+  public getFragmentShaderSource() {
+    return `
 precision highp float;
 uniform samplerCube uSampler;
 varying highp vec3 vVertexDirectionVector;
 void main(void) {
-	gl_FragColor = textureCube(uSampler, vVertexDirectionVector);
+  gl_FragColor = textureCube(uSampler, vVertexDirectionVector);
 }`;
-	}
+  }
 
-	public updateTexture(gl: WebGLRenderingContext, image, imageConfig) {
-		const baseOrder = "RLUDBF";
-		const order = CubeRenderer.extractOrder(imageConfig);
-		const orderMap = {};
+  public updateTexture(gl: WebGLRenderingContext, image: HTMLImageElement | HTMLVideoElement, imageConfig: CubemapConfig) {
+    const baseOrder = "RLUDBF";
+    const order = CubeRenderer.extractOrder(imageConfig);
+    const orderMap = {};
 
-		order.split("").forEach((v, i) => {
-			orderMap[v] = i;
-		});
+    order.split("").forEach((v, i) => {
+      orderMap[v] = i;
+    });
 
-		try {
-			if (image instanceof Array) {
-				for (let surfaceIdx = 0; surfaceIdx < 6; surfaceIdx++) {
-					const tileIdx = orderMap[baseOrder[surfaceIdx]];
+    try {
+      if (image instanceof Array) {
+        for (let surfaceIdx = 0; surfaceIdx < 6; surfaceIdx++) {
+          const tileIdx = orderMap[baseOrder[surfaceIdx]];
 
-					WebGLUtils.texImage2D(gl, gl.TEXTURE_CUBE_MAP_POSITIVE_X + surfaceIdx, image[tileIdx]);
-				}
-			} else {
-				const maxCubeMapTextureSize = this.getMaxCubeMapTextureSize(gl, image);
+          WebGLUtils.texImage2D(gl, gl.TEXTURE_CUBE_MAP_POSITIVE_X + surfaceIdx, image[tileIdx]);
+        }
+      } else {
+        const maxCubeMapTextureSize = this.getMaxCubeMapTextureSize(gl, image);
 
-				for (let surfaceIdx = 0; surfaceIdx < 6; surfaceIdx++) {
-					const tileIdx = orderMap[baseOrder[surfaceIdx]];
-					const tile = this.extractTileFromImage(
-						image, tileIdx, maxCubeMapTextureSize
-					);
+        for (let surfaceIdx = 0; surfaceIdx < 6; surfaceIdx++) {
+          const tileIdx = orderMap[baseOrder[surfaceIdx]];
+          const tile = this.extractTileFromImage(
+            image, tileIdx, maxCubeMapTextureSize
+          );
 
-					WebGLUtils.texImage2D(gl, gl.TEXTURE_CUBE_MAP_POSITIVE_X + surfaceIdx, tile);
-				}
-			}
-		} catch (e: any) {
-			this._triggerError(e);
-		}
-	}
+          WebGLUtils.texImage2D(gl, gl.TEXTURE_CUBE_MAP_POSITIVE_X + surfaceIdx, tile);
+        }
+      }
+    } catch (e: any) {
+      this._triggerError(e);
+    }
+  }
 
-	public bindTexture(gl: WebGLRenderingContext, texture, image, imageConfig: CubemapConfig) {
-		gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
-		this.updateTexture(gl, image, imageConfig);
-	}
+  public bindTexture(gl: WebGLRenderingContext, texture: WebGLTexture, image: HTMLImageElement | HTMLVideoElement, imageConfig: CubemapConfig) {
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+    this.updateTexture(gl, image, imageConfig);
+  }
 
-	public getSourceTileSize(image: HTMLImageElement | HTMLVideoElement) {
-		const {width, height} = this.getDimension(image);
-		const aspectRatio = width / height;
-		let inputTextureSize;
+  public getSourceTileSize(image: HTMLImageElement | HTMLVideoElement) {
+    const {width, height} = this.getDimension(image);
+    const aspectRatio = width / height;
+    let inputTextureSize;
 
-		if (aspectRatio === 1 / 6) {
-			inputTextureSize = width;
-		} else if (aspectRatio === 6) {
-			inputTextureSize = height;
-		} else if (aspectRatio === 2 / 3) {
-			inputTextureSize = width / 2;
-		} else {
-			inputTextureSize = width / 3;
-		}
-		return inputTextureSize;
-	}
+    if (aspectRatio === 1 / 6) {
+      inputTextureSize = width;
+    } else if (aspectRatio === 6) {
+      inputTextureSize = height;
+    } else if (aspectRatio === 2 / 3) {
+      inputTextureSize = width / 2;
+    } else {
+      inputTextureSize = width / 3;
+    }
+    return inputTextureSize;
+  }
 
-	public extractTileFromImage(image: HTMLImageElement | HTMLVideoElement, tileIdx: number, outputTextureSize: number) {
-		const {width} = this.getDimension(image);
-		const inputTextureSize = this.getSourceTileSize(image);
+  public extractTileFromImage(image: HTMLImageElement | HTMLVideoElement, tileIdx: number, outputTextureSize: number) {
+    const {width} = this.getDimension(image);
+    const inputTextureSize = this.getSourceTileSize(image);
 
-		const canvas = document.createElement("canvas");
+    const canvas = document.createElement("canvas");
 
-		canvas.width = outputTextureSize;
-		canvas.height = outputTextureSize;
-		const context = canvas.getContext("2d");
-		const tilePerRow = width / inputTextureSize;
+    canvas.width = outputTextureSize;
+    canvas.height = outputTextureSize;
+    const context = canvas.getContext("2d");
+    const tilePerRow = width / inputTextureSize;
 
-		const x = inputTextureSize * tileIdx % (inputTextureSize * tilePerRow);
-		const y = Math.floor(tileIdx / tilePerRow) * (inputTextureSize);
+    const x = inputTextureSize * tileIdx % (inputTextureSize * tilePerRow);
+    const y = Math.floor(tileIdx / tilePerRow) * (inputTextureSize);
 
-		context!.drawImage(
-			image, x, y,
-			inputTextureSize, inputTextureSize, 0, 0, outputTextureSize, outputTextureSize
-		);
-		return canvas;
-	}
+    context!.drawImage(
+      image, x, y,
+      inputTextureSize, inputTextureSize, 0, 0, outputTextureSize, outputTextureSize
+    );
+    return canvas;
+  }
 
-	public getMaxCubeMapTextureSize(gl: WebGLRenderingContext, image: HTMLImageElement | HTMLVideoElement) {
-		const agent = Agent();
-		const maxCubeMapTextureSize = gl.getParameter(gl.MAX_CUBE_MAP_TEXTURE_SIZE);
-		let _imageWidth = this.getSourceTileSize(image);
+  public getMaxCubeMapTextureSize(gl: WebGLRenderingContext, image: HTMLImageElement | HTMLVideoElement) {
+    const agent = Agent();
+    const maxCubeMapTextureSize = gl.getParameter(gl.MAX_CUBE_MAP_TEXTURE_SIZE);
+    let _imageWidth = this.getSourceTileSize(image);
 
-		if (agent.browser.name === "ie" && agent.browser.majorVersion === 11) {
-			if (!mathUtil.isPowerOfTwo(_imageWidth)) {
-				for (let i = 1; i < maxCubeMapTextureSize; i *= 2) {
-					if (i < _imageWidth) {
-						continue;
-					} else {
-						_imageWidth = i;
-						break;
-					}
-				}
-			}
-		}
-		if (agent.os.name === "ios") {
-			const majorVersion = agent.os.majorVersion;
+    if (agent.browser.name === "ie" && agent.browser.majorVersion === 11) {
+      if (!mathUtil.isPowerOfTwo(_imageWidth)) {
+        for (let i = 1; i < maxCubeMapTextureSize; i *= 2) {
+          if (i < _imageWidth) {
+            continue;
+          } else {
+            _imageWidth = i;
+            break;
+          }
+        }
+      }
+    }
+    if (agent.os.name === "ios") {
+      const majorVersion = agent.os.majorVersion;
 
-			// ios 9 의 경우 텍스쳐 최대사이즈는 1024 이다.
-			if (majorVersion === 9) {
-				_imageWidth = 1024;
-			}
-			// ios 8 의 경우 텍스쳐 최대사이즈는 512 이다.
-			if (majorVersion === 8) {
-				_imageWidth = 512;
-			}
-		}
-		// maxCubeMapTextureSize 보다는 작고, imageWidth 보다 큰 2의 승수 중 가장 작은 수
-		return Math.min(maxCubeMapTextureSize, _imageWidth);
-	}
+      // ios 9 의 경우 텍스쳐 최대사이즈는 1024 이다.
+      if (majorVersion === 9) {
+        _imageWidth = 1024;
+      }
+      // ios 8 의 경우 텍스쳐 최대사이즈는 512 이다.
+      if (majorVersion === 8) {
+        _imageWidth = 512;
+      }
+    }
+    // maxCubeMapTextureSize 보다는 작고, imageWidth 보다 큰 2의 승수 중 가장 작은 수
+    return Math.min(maxCubeMapTextureSize, _imageWidth);
+  }
 }
 
 export default CubeRenderer;
--- src/PanoImageRenderer/renderer/Renderer.ts
+++ src/PanoImageRenderer/renderer/Renderer.ts
@@ -9,14 +9,14 @@ const isIE11 = agent.browser.name === "ie" && agent.browser.majorVersion === 11;
 const EVENTS: {
   ERROR: "error",
 } = {
-	ERROR: "error"
+  ERROR: "error"
 };
 
 /**
  *
  * Extends Component for firing errors occurs internally.
  */
-class Renderer extends Component<{
+abstract class Renderer extends Component<{
   [EVENTS.ERROR]: {
     message: string;
   }
@@ -27,146 +27,154 @@ class Renderer extends Component<{
   private _pixelCanvas: HTMLCanvasElement | null;
   private _pixelContext: CanvasRenderingContext2D | null;
 
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this._forceDimension = null;
-		this._pixelCanvas = null;
-		this._pixelContext = null;
-	}
+    this._forceDimension = null;
+    this._pixelCanvas = null;
+    this._pixelContext = null;
+  }
 
-	public render({ gl, shaderProgram, indexBuffer, mvMatrix, pMatrix }: {
+  public render({ gl, shaderProgram, indexBuffer, mvMatrix, pMatrix }: {
     gl: WebGLRenderingContext;
     shaderProgram: WebGLProgram;
     indexBuffer: WebGLBuffer;
     mvMatrix: mat4;
     pMatrix: mat4;
   }) {
-		gl.uniformMatrix4fv((shaderProgram as any).pMatrixUniform, false, pMatrix);
-		gl.uniformMatrix4fv((shaderProgram as any).mvMatrixUniform, false, mvMatrix);
+    gl.uniformMatrix4fv((shaderProgram as any).pMatrixUniform, false, pMatrix);
+    gl.uniformMatrix4fv((shaderProgram as any).mvMatrixUniform, false, mvMatrix);
 
-		if (indexBuffer) {
-			gl.drawElements(gl.TRIANGLES, (indexBuffer as any).numItems, gl.UNSIGNED_SHORT, 0);
-		}
-	}
+    if (indexBuffer) {
+      gl.drawElements(gl.TRIANGLES, (indexBuffer as any).numItems, gl.UNSIGNED_SHORT, 0);
+    }
+  }
 
-	// Define interface for Renderers
-	/**
-	 * Following MUST BE DEFINED on Child of Renderer
-	 *
-	 * DATA
-	 *
-	 *  - getVertexPositionData
-	 *  - getIndexData
-	 *  - getTextureCoordData
-	 *
-	 * SOURCE
-	 *
-	 *  - getVertexShaderSource
-	 *  - getFragmentShaderSource
-	 *
-	 * TEXTURE
-	 *
-	 *  - bindTexture
-	 */
-	public getDimension(pixelSource: HTMLImageElement | HTMLVideoElement) {
+  // Define interface for Renderers
+  /**
+   * Following MUST BE DEFINED on Child of Renderer
+   *
+   * DATA
+   *
+   *  - getVertexPositionData
+   *  - getIndexData
+   *  - getTextureCoordData
+   *
+   * SOURCE
+   *
+   *  - getVertexShaderSource
+   *  - getFragmentShaderSource
+   *
+   * TEXTURE
+   *
+   *  - bindTexture
+   */
+  public getDimension(pixelSource: HTMLImageElement | HTMLVideoElement) {
     const width = (pixelSource as HTMLImageElement).naturalWidth
       || (pixelSource as HTMLVideoElement).videoWidth;
     const height = (pixelSource as HTMLImageElement).naturalHeight
       || (pixelSource as HTMLVideoElement).videoHeight;
 
-		return { width, height };
-	}
+    return { width, height };
+  }
 
-	/**
-	 * Update data used by shader
-	 */
-	public updateShaderData(param) {
-		/*
-		* Update following data in implementation layer.
-		* If the data is not changed, it does not need to implement this function.
-		*
-		* - _VERTEX_POSITION_DATA
-		* - _TEXTURE_COORD_DATA
-		* - _INDEX_DATA
-		*/
-	}
+  /**
+   * Update data used by shader
+   */
+  public updateShaderData(param) {
+    /*
+    * Update following data in implementation layer.
+    * If the data is not changed, it does not need to implement this function.
+    *
+    * - _VERTEX_POSITION_DATA
+    * - _TEXTURE_COORD_DATA
+    * - _INDEX_DATA
+    */
+  }
 
-	/**
-	 *
-	 * @param {HTMLImageElement | HTMLVideoElement} image
-	 * @param {Object = {width, height}} forceDimension Forced dimension to resize
-	 */
-	protected _initPixelSource(image: HTMLImageElement | HTMLVideoElement, forceDimension: Renderer["_forceDimension"] = null) {
-		const isIE11Video = isIE11 && (image instanceof HTMLVideoElement);
+  public abstract getVertexPositionData(): number[];
+  public abstract getIndexData(): number[];
+  public abstract getTextureCoordData(imageConfig?: CubemapConfig): number[];
+  public abstract getVertexShaderSource(): string;
+  public abstract getFragmentShaderSource(): string;
+  public abstract bindTexture(gl: WebGLRenderingContext, texture: WebGLTexture, image: HTMLImageElement | HTMLVideoElement, imageConfig?: CubemapConfig): void;
+  public abstract updateTexture(gl: WebGLRenderingContext, image: HTMLImageElement | HTMLVideoElement, imageConfig?: CubemapConfig): void;
 
-		if (isIE11Video || forceDimension) {
-			const {width, height} = forceDimension || this.getDimension(image);
+  /**
+   *
+   * @param {HTMLImageElement | HTMLVideoElement} image
+   * @param {Object = {width, height}} forceDimension Forced dimension to resize
+   */
+  protected _initPixelSource(image: HTMLImageElement | HTMLVideoElement, forceDimension: Renderer["_forceDimension"] = null) {
+    const isIE11Video = isIE11 && (image instanceof HTMLVideoElement);
 
-			this._pixelCanvas = document.createElement("canvas");
-			this._pixelCanvas.width = width;
-			this._pixelCanvas.height = height;
-			this._pixelContext = this._pixelCanvas.getContext("2d");
-		}
-		this._forceDimension = forceDimension;
-	}
+    if (isIE11Video || forceDimension) {
+      const {width, height} = forceDimension || this.getDimension(image);
 
-	protected _getPixelSource(image: HTMLImageElement | HTMLVideoElement) {
-		if (!this._pixelCanvas) {
-			return image;
-		}
+      this._pixelCanvas = document.createElement("canvas");
+      this._pixelCanvas.width = width;
+      this._pixelCanvas.height = height;
+      this._pixelContext = this._pixelCanvas.getContext("2d");
+    }
+    this._forceDimension = forceDimension;
+  }
 
-		/**
-		 * IE11 && Video
-		 * or
-		 * Dimension is forced (Image is larger than texture size.)
-		 */
-		const contentDimension = this.getDimension(image);
-		const textureDimension = this._forceDimension || contentDimension;
+  protected _getPixelSource(image: HTMLImageElement | HTMLVideoElement) {
+    if (!this._pixelCanvas) {
+      return image;
+    }
 
-		if (this._pixelCanvas.width !== textureDimension.width) {
-			this._pixelCanvas.width = textureDimension.width;
-		}
+    /**
+     * IE11 && Video
+     * or
+     * Dimension is forced (Image is larger than texture size.)
+     */
+    const contentDimension = this.getDimension(image);
+    const textureDimension = this._forceDimension || contentDimension;
 
-		if (this._pixelCanvas.height !== textureDimension.height) {
-			this._pixelCanvas.height = textureDimension.height;
-		}
+    if (this._pixelCanvas.width !== textureDimension.width) {
+      this._pixelCanvas.width = textureDimension.width;
+    }
 
-		if (this._forceDimension) {
-			this._pixelContext!.drawImage(image,
-				0, 0, contentDimension.width, contentDimension.height,
-				0, 0, textureDimension.width, textureDimension.height);
-		} else {
-			this._pixelContext!.drawImage(image, 0, 0);
-		}
+    if (this._pixelCanvas.height !== textureDimension.height) {
+      this._pixelCanvas.height = textureDimension.height;
+    }
 
-		return this._pixelCanvas;
-	}
+    if (this._forceDimension) {
+      this._pixelContext!.drawImage(image,
+        0, 0, contentDimension.width, contentDimension.height,
+        0, 0, textureDimension.width, textureDimension.height);
+    } else {
+      this._pixelContext!.drawImage(image, 0, 0);
+    }
 
-	protected _extractTileConfig(imageConfig: CubemapConfig) {
-		let tileConfig: TileConfig[] =
-			Array.isArray(imageConfig.tileConfig) ?
-				imageConfig.tileConfig : Array(...Array(6)).map(() => imageConfig.tileConfig) as TileConfig[];
+    return this._pixelCanvas;
+  }
 
-		tileConfig = tileConfig.map(
-			config => Object.assign({
-				flipHorizontal: false,
-				rotation: 0
-			}, config)
-		);
+  protected _extractTileConfig(imageConfig: CubemapConfig) {
+    let tileConfig: TileConfig[] =
+      Array.isArray(imageConfig.tileConfig) ?
+        imageConfig.tileConfig : Array(...Array(6)).map(() => imageConfig.tileConfig) as TileConfig[];
 
-		return tileConfig;
-	}
+    tileConfig = tileConfig.map(
+      config => Object.assign({
+        flipHorizontal: false,
+        rotation: 0
+      }, config)
+    );
 
-	protected _triggerError(error) {
-		/* eslint-disable no-console */
-		console.error("Renderer Error:", error);
-		/* eslint-enable no-console */
+    return tileConfig;
+  }
 
-		this.trigger(EVENTS.ERROR, {
-			message: typeof error === "string" ? error : error.message
-		});
-	}
+  protected _triggerError(error) {
+    /* eslint-disable no-console */
+    console.error("Renderer Error:", error);
+    /* eslint-enable no-console */
+
+    this.trigger(EVENTS.ERROR, {
+      message: typeof error === "string" ? error : error.message
+    });
+  }
 }
 
 export default Renderer;
