63f5f7dbf4e2804226f37e90b93f3894c7e18502
WoodNeck <saiyu915@naver.com>
2020-10-28 19:13:26 +0900

    chore: configure build script

---
+++ src/SpinViewer/index.umd.ts
@@ -0,0 +1,9 @@
+import SpinViewer from "./SpinViewer";
+import SpriteImage from "./SpriteImage";
+import { VERSION } from "../version";
+
+export default {
+  SpinViewer,
+  SpriteImage,
+  VERSION,
+}
--- src/index.umd.ts
+++ src/index.umd.ts
@@ -2,8 +2,8 @@ import PanoViewer from "./PanoViewer/PanoViewer";
 import { SpinViewer, SpriteImage } from "./SpinViewer";
 import { VERSION } from "./version";
 
-export {
-	PanoViewer,
+export default {
+  PanoViewer,
 	SpinViewer,
 	SpriteImage,
 	VERSION
