c62bacf68ab1b8d906a2fa1dbd66c30178532fdb
WoodNeck <saiyu915@naver.com>
2021-10-12 20:26:40 +0900

    Updates

--- common/js/PanoControls.js
+++ common/js/PanoControls.js
@@ -105,7 +105,7 @@ var PanoControls = (function() {
 
 		var vrButton = document.querySelector(".entervr")
 		vrButton && vrButton.addEventListener("click", function() {
-			panoViewer.enterVR().catch(e => {
+			panoViewer.enterVR().catch(function (e) {
 				Swal.fire({
 					title: "Can't enter VR mode!",
 					text: e.message || e,
@@ -115,7 +115,7 @@ var PanoControls = (function() {
 		});
 
 		// For iOS 13+
-		panoViewer.enableSensor().catch(() => {
+		panoViewer.enableSensor().catch(function () {
 			Swal.fire({
 				title: "Permission needed! (iOS13+)",
 				icon: "question",
@@ -124,10 +124,10 @@ var PanoControls = (function() {
 				reverseButtons: true,
 				confirmButtonText: "Allow",
 				cancelButtonText: "Deny"
-			}).then(result => {
+			}).then(function (result) {
 				if (result.value) {
 					// Granted
-					panoViewer.enableSensor().catch(() => {
+					panoViewer.enableSensor().catch(function () {
 						Swal.fire(
 							"You've denied a permission!",
 							"You have to completely close out your browser and reconnect this page to enable sensor again!",
@@ -140,7 +140,7 @@ var PanoControls = (function() {
 			});
 		});
 
-		const iOSVersion = (() => {
+		const iOSVersion = (function () {
 			if (/iP(hone|od|ad)/.test(navigator.platform)) {
 				// supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
 				var v = navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
@@ -150,26 +150,26 @@ var PanoControls = (function() {
 
 		// For iOS 12.2 ~ 13
 		if (iOSVersion && iOSVersion[0] === 12 && iOSVersion[1] >= 2) {
-			PanoViewer.isGyroSensorAvailable(available => {
+			PanoViewer.isGyroSensorAvailable(function (available) {
 				if (!available) {
 					Swal.fire({
 						title: "Please enable the Motion Sensor! (iOS12.2~13)",
 						icon: "warning",
 						text: "This website requires a permission for your device motion sensor for the immersive experience.",
-						html: `
-						<div style="text-align: left;">
-							<div>
-							1. Open <img src="https://developer.apple.com/design/human-interface-guidelines/ios/images/icons/app_icons/Settings_2x.png" width="20" /> <b>Settings</b>
-							</div>
-							<div>
-							2. Select <img src="https://km.support.apple.com/resources/sites/APPLE/content/live/IMAGES/0/IM26/en_US/safari-240.png" width="20" /> <b>Safari</b>
-							</div>
-							<div>
-							3. Enable <b>Motion & Orientation Access</b>
-							</div>
-							<div>4. Reload the page</div>
-						</div>
-						`
+						html: "\
+						<div style=\"text-align: left;\">\
+							<div>\
+							1. Open <img src=\"https://developer.apple.com/design/human-interface-guidelines/ios/images/icons/app_icons/Settings_2x.png\" width=\"20\" /> <b>Settings</b>\
+							</div>\
+							<div>\
+							2. Select <img src=\"https://km.support.apple.com/resources/sites/APPLE/content/live/IMAGES/0/IM26/en_US/safari-240.png\" width=\"20\" /> <b>Safari</b>\
+							</div>\
+							<div>\
+							3. Enable <b>Motion & Orientation Access</b>\
+							</div>\
+							<div>4. Reload the page</div>\
+						</div>\
+						"
 					});
 				}
 			});
