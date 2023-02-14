e7b48ac37fb8fbe5abce89afce05bebf34014ba8
WoodNeck <saiyu915@naver.com>
2023-02-14 16:59:36 +0900

    fix: svelte SSR

--- packages/svelte-view360/src/View360.js
+++ packages/svelte-view360/src/View360.js
@@ -2,6 +2,8 @@ import { withMethods } from "@egjs/view360";
 
 import View360 from "./View360.svelte";
 
-withMethods(View360.prototype, "view360");
+if (View360.prototype) {
+  withMethods(View360.prototype, "view360");
+}
 
 export default View360;
