f0df40989150bee402e0f15a7725fc37c888d4df
WoodNeck <saiyu915@naver.com>
2023-02-16 14:16:42 +0900

    docs: change main ansel images url

--- demo/src/components/home/main-pano-items.ts
+++ demo/src/components/home/main-pano-items.ts
@@ -1,59 +1,62 @@
 const BF5 = [
-  "https://lh5.googleusercontent.com/ArAfFcFzmsdRyJhATy-5OuVZpsAR8hMNnmAwYAX21LaV3tFUs8aindTtdrfi6WyZO6s=w4096",
-  "https://lh5.googleusercontent.com/3PPVqGt3GsAeEmTtpoeTRGadfa8T0oPHXavCSFgCMg2s3Oo6kct3lXo-vYRDOMvMLwg=w4096",
-  "https://lh5.googleusercontent.com/QO435LXnE3X4du6aAyAyc7cp5dnsu-RpRSe9xtYLMTz_wxI_5cvedaPtBMvB0BH9qeY=w4096"
+  "HGJRZy7",
+  "HGJRQvS",
+  "HGJRLa2"
 ];
 
 const DRG = [
-  "https://lh5.googleusercontent.com/Y1W2bweq0tKEljt1bUL-V57i4sNaT-GRS57EXEN8a9IjCLnlJl5VLLmOeUzKGaAYL8Q=w4096"
+  "HGJ5hN9"
 ];
 
 const DQ11 = [
-  "https://lh5.googleusercontent.com/QXnfmLqdcPbu6StpGj6Z4qY7xMN8_y8MTYHRklvPviA4JhDkVUqphC2sZLGWcvkcgGY=w4096",
-  "https://lh5.googleusercontent.com/ZYi5jSmgHoeMYI_nctIZoCCz3UAxpE1Xi3pGHYJsDyz0yhYAx2CeaZTUiY40iPU0rvE=w4096",
-  "https://lh5.googleusercontent.com/aB7Wber3gjt6ItyCoPe51d9H60om3Wn-vCvnGa0yZ2JUkSxO8vXUWjk5a89EQbTogCM=w4096",
-  "https://lh5.googleusercontent.com/iTax1ISbD0mqXinrdSH9rcEB2mIZr_7wO5JNxeVvQXiUAsP0njEn85DGF4bb1CLKFdQ=w4096",
-  "https://lh5.googleusercontent.com/-YTyuzMCOzNMP39oFBQi8Bov8JMtjmFsT0ab1JO8kYUAEQASfjgtd9SKCp8jI7_t25Y=w4096",
-  "https://lh5.googleusercontent.com/Ann9eYS3K-p9ANqWC4pqpEzqSExDRLjQ3lsWIkyQxKwg0cyqKBpZm8l-Z-eUmSzb1IQ=w4096",
-  "https://lh5.googleusercontent.com/I5cqOYMIYCVE1V7whINZqx4z0mmldHKiq48HREMQLAbZaiKFTphNN7By5F3dGmh0ysM=w4096",
-  "https://lh5.googleusercontent.com/RAM4ZwHy27SOeYN_BhEHkqjl01L9z7Mhxhl67_aB1dzp-oIHVYe0mtUcWNbIP4u8d78=w4096"
+  "HGJ5ZKv",
+  "HGJ5sVa",
+  "HGJ5LiJ",
+  "HGJ5tlR",
+  "HGJ5DSp",
+  "HGJ5mHN",
+  "HGJ5pRI",
+  "HGJ5yNt"
 ];
 
 const FF15 = [
-  "https://lh5.googleusercontent.com/ypRdxqu_3tQj13YXau5tFMvl2inKWoTUKVarki9ukd3wEBUfBoZPNiDtceXuUFrjU7Y=w4096",
-  "https://lh5.googleusercontent.com/9aeGamO-3Ha9COyE84hMYNKOLKPlDaoH9_3GcBwxMDfWAyxQ1MsPNUcqQ2oJawzJiv4=w4096",
-  "https://lh5.googleusercontent.com/1Rxnw9UZGP8Yud7jEJQ2ILgaZMnzEmEUUzjGrqGYvoSz02xm6Y2ZXvWmet2IjIGjSN4=w4096",
-  "https://lh5.googleusercontent.com/mI1x9GlAf6pIwBboGMefRcwfbSlN3rO_-xh3hdLzXBJsclZI4NYkbbbMEcm1Iqdg09Q=w4096"
+  "HGJWyOb",
+  "HGJXJzx",
+  "HGJXdWQ",
+  "HGJX9bj"
 ];
 
 const FOR_HONOR = [
-  "https://lh5.googleusercontent.com/tO0cMP09ZR9MvcR89jTkNPegUwY20_FsLmB1NIT3euVjGr-T2ERBvUasCN_cGcGr-YE=w4096",
-  "https://lh5.googleusercontent.com/5xm12NPQynjp2IVJZENDykvn3m9IULuIZcXIISzsAkx8I2ibVOcgQ2SsFd08_evWjsk=w4096",
-  "https://lh5.googleusercontent.com/w1GCNgGzON-q6PR7PwHGXS9C2qp5WCEnOzhfX2GvMzLTU8gatSfNyV8-B1Wrg-zmiZ0=w4096",
-  "https://lh5.googleusercontent.com/US7jM4N3PRXqrkPUHFn9TL6aekNMbHaBL7LlJ1EF7N3LNNewuD3vmK5gV4kkj2BSg7A=w4096"
+  "HGJXzzv",
+  "HGJXC5g",
+  "HGJXomJ",
+  "HGJXnea"
 ];
 
 const QUBE2 = [
-  "https://lh5.googleusercontent.com/9otNmgxns5Dvi01PYg1Mo0CF9ZJkPqV5MNEPhXYCiwo93V5ZWwWyJmQu31syzMG9yYo=w4096",
-  "https://lh5.googleusercontent.com/SGz1HFXr5iAy0k8ETe0i8laSWTtXi-XTzrxuQzKpKdOQHcrPcPH7GqanKfAKnVfZzsc=w4096"
+  "HGJXR1I",
+  "HGJXAqN"
 ];
 
 const WITNESS = [
-  "https://lh5.googleusercontent.com/5UuUC4sFVpMYB7eVmjbR04j6wa_UGJ_bP35KHaYIAzvdesx38bJKSoptIq6BDf1l4sg=w4096",
-  "https://lh5.googleusercontent.com/AIZnLMILr1vVIoNqg8eGxGSFvp5GrICyy6o4XHEqMIXo0_mgrn0AjOGxqOw0OVsyQ-A=w4096",
-  "https://lh5.googleusercontent.com/xvwuqYoqQvOlkMyaOPB4apyD9tY4zlVPZGmY_vXWWzfl33AhXfPw1Ca-BZTd8ACzAmQ=w4096",
-  "https://lh5.googleusercontent.com/Ze0uByvH2KBA9TYmagDRTaSq2E6wxQ_aZHVvwmiKlcjY1d1DlJZ_85WbV2izvXKiNpY=w4096",
-  "https://lh5.googleusercontent.com/8DT09DUnM4iPjgmHOFLM3Ls01LT6ZUHDq6DI-ajQsIYRNHj1pa_mgkPkAdsSVCVHE-4=w4096",
-  "https://lh5.googleusercontent.com/8DMRwe_NTsvKo4YwCbsL6zRu6GtCyol4qQRGa5UJ7QEAcaUuk7CQ2Gw7WAwlPxKx4IU=w4096",
-  "https://lh5.googleusercontent.com/hMUUQim4N47l1FVDD-LwK3FjnBtZeZGdFGSPVlGt_7YzkfBdNKfx0XQvRa1_qJZa-Y8=w4096"
+  "HGJXlmG",
+  "HGJXEX4",
+  "HGJXGLl",
+  "HGJX1If",
+  "HGJXVB2",
+  "HGJXW1S",
+  "HGJXXr7"
 ];
 
+// https://freeimage.host/
+const bindImageSrc = src => `https://iili.io/${src}.jpg`;
+
 export default [
-  ...BF5.map(src => ({ src, license: "battlefieldV" })),
-  ...DRG.map(src => ({ src, license: "drg" })),
-  ...DQ11.map(src => ({ src, license: "dq11" })),
-  ...FF15.map(src => ({ src, license: "ff15" })),
-  ...FOR_HONOR.map(src => ({ src, license: "for_honor" })),
-  ...QUBE2.map(src => ({ src, license: "qube2" })),
-  ...WITNESS.map(src => ({ src, license: "witness" }))
+  ...BF5.map(src => ({ src: bindImageSrc(src), license: "battlefieldV" })),
+  ...DRG.map(src => ({ src: bindImageSrc(src), license: "drg" })),
+  ...DQ11.map(src => ({ src: bindImageSrc(src), license: "dq11" })),
+  ...FF15.map(src => ({ src: bindImageSrc(src), license: "ff15" })),
+  ...FOR_HONOR.map(src => ({ src: bindImageSrc(src), license: "for_honor" })),
+  ...QUBE2.map(src => ({ src: bindImageSrc(src), license: "qube2" })),
+  ...WITNESS.map(src => ({ src: bindImageSrc(src), license: "witness" }))
 ];
