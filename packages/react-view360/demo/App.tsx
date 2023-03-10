a44248befbf0a30215b5e566bae6f3991119395a
WoodNeck <saiyu915@naver.com>
2023-03-10 14:51:49 +0900

    fix: ControlBar not removing child elements

--- packages/react-view360/demo/App.tsx
+++ packages/react-view360/demo/App.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from "react";
-import View360, { EquirectProjection } from "../src";
+import View360, { ControlBar, EquirectProjection } from "../src";
 import "./App.css";
 import "@egjs/view360/css/view360.min.css";
 
@@ -7,8 +7,10 @@ export default () => {
   const projection = useMemo(() => new EquirectProjection({
     src: "https://iili.io/HGJXGLl.jpg"
   }), []);
+  const plugins = useMemo(() => [new ControlBar()], []);
 
   return <View360
     projection={projection}
+    plugins={plugins}
     className="is-3by1" />;
 };
