6e31e939a0ff055934ead7e63b4d743243590ac1
WoodNeck <saiyu915@naver.com>
2023-03-24 14:44:08 +0900

    fix: create cjs build and fix wrong exports

--- packages/vue3-view360/src/index.umd.ts
+++ packages/vue3-view360/src/index.umd.ts
@@ -5,6 +5,11 @@
 import { App } from "vue";
 
 import View360 from "./View360";
+import * as modules from "@egjs/view360";
+
+for (const name in modules) {
+  (View360 as any)[name] = (modules as any)[name];
+}
 
 export default {
   install: (app: App) => {
